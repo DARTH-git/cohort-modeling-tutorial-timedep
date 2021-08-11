ecb912a24e08c779470edd6d902b9e260989f44e
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2021-08-10 20:21:46 -0500

    Update cSTM_time_dep_simulation.R
    Fixed bug on timing of parallelized PSA

--- analysis/cSTM_time_dep_simulation.R
+++ analysis/cSTM_time_dep_simulation.R
@@ -511,13 +511,15 @@ print(paste0("PSA with ", comma(n_sim), " simulations run in series in ",
              units(n_time_total_psa_series)))
 
 ### Run Markov model on each parameter set of PSA input dataset in parallel
+## Uncomment next section to run in parallel
 # ## Get OS
 # os <- get_os()
-# print(paste0("Parallelized PSA on ", os))
 # 
 # no_cores <- parallel::detectCores() - 1
 # 
-# n_time_init_psa <- Sys.time()
+# print(paste0("Parallelized PSA on ", os, " using", no_cores, " cores."))
+# 
+# n_time_init_psa_parallel <- Sys.time()
 # 
 # ## Run parallelized PSA based on OS
 # if(os == "macosx"){
@@ -534,7 +536,7 @@ print(paste0("PSA with ", comma(n_sim), " simulations run in series in ",
 #   df_c <- df_ce[, 1:n_str]
 #   df_e <- df_ce[, (n_str+1):(2*n_str)]
 #   # Register end time of parallelized PSA
-#   n_time_end_psa <- Sys.time()
+#   n_time_end_psa_parallel <- Sys.time()
 # }
 # if(os == "windows"){
 #   # Initialize cluster object
@@ -544,17 +546,17 @@ print(paste0("PSA with ", comma(n_sim), " simulations run in series in ",
 #   opts <- list(attachExportEnv = TRUE)
 #   # Run parallelized PSA
 #   df_ce <- foreach::foreach(i = 1:n_samp, .combine = rbind,
-#                            .export = ls(globalenv()),
-#                            .packages=c("dampack"),
-#                            .options.snow = opts) %dopar% {
-#                              l_out_temp <- calculate_ce_out(df_psa_input[i, ])
-#                              df_ce <- c(l_out_temp$Cost, l_out_temp$Effect)
-#                            }
+#                             .export = ls(globalenv()),
+#                             .packages=c("dampack"),
+#                             .options.snow = opts) %dopar% {
+#                               l_out_temp <- calculate_ce_out(df_psa_input[i, ])
+#                               df_ce <- c(l_out_temp$Cost, l_out_temp$Effect)
+#                             }
 #   # Extract costs and effects from the PSA dataset
 #   df_c <- df_ce[, 1:n_str]
 #   df_e <- df_ce[, (n_str+1):(2*n_str)]
 #   # Register end time of parallelized PSA
-#   n_time_end_psa <- Sys.time()
+#   n_time_end_psa_parallel <- Sys.time()
 # }
 # if(os == "linux"){
 #   # Initialize cluster object
@@ -570,14 +572,14 @@ print(paste0("PSA with ", comma(n_sim), " simulations run in series in ",
 #   df_c <- df_ce[, 1:n_str]
 #   df_e <- df_ce[, (n_str+1):(2*n_str)]
 #   # Register end time of parallelized PSA
-#   n_time_end_psa <- Sys.time()
+#   n_time_end_psa_parallel <- Sys.time()
 # }
 # # Stop clusters
 # stopCluster(cl)
-# n_time_total_psa <- n_time_end_psa - n_time_init_psa
+# n_time_total_psa_parallel <- n_time_end_psa_parallel - n_time_init_psa_parallel
 # print(paste0("PSA with ", comma(n_sim), " simulations run in series in ",
-#              round(n_time_total_psa, 2), " ", 
-#              units(n_time_total_psa_series)))
+#              round(n_time_total_psa_parallel, 2), " ",
+#              units(n_time_total_psa_parallel)))
 
 # Create PSA object for dampack
 l_psa <- make_psa_obj(cost          = df_c, 
