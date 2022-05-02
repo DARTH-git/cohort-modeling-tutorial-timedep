d9c5af02daddefe573e33b8953df9d44cbcda235
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2022-05-01 23:15:01 -0500

    Update Functions.R
    Added function to summarize output from PSA for epi measures

--- R/Functions.R
+++ R/Functions.R
@@ -2101,3 +2101,31 @@ get_os <- function(){
   }
   tolower(os)
 }
+
+#---------------------------------------------------# 
+##### Function to summarize posterior output  #######
+#---------------------------------------------------# 
+data_summary <- function(data, varname, groupnames){
+  #+++++++++++++++++++++++++
+  # Function to calculate the mean, standard deviation and 95% Credible Interval
+  # for each group
+  # Source: http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
+  #+++++++++++++++++++++++++
+  # data : a data frame
+  # varname : the name of a column containing the variable
+  #to be summariezed
+  # groupnames : vector of column names to be used as
+  # grouping variables
+  require(plyr)
+  summary_func <- function(x, col){
+    c(mean = mean(x[[col]], na.rm=TRUE),
+      median = quantile(x[[col]], probs = 0.5, names = FALSE),
+      sd = sd(x[[col]], na.rm=TRUE),
+      lb = quantile(x[[col]], probs = 0.025, names = FALSE),
+      ub = quantile(x[[col]], probs = 0.975, names = FALSE))
+  }
+  data_sum <- ddply(data, groupnames, .fun=summary_func, 
+                    varname)
+  data_sum <- plyr::rename(data_sum, c("mean" = varname))
+  return(data_sum)
+}
